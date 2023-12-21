88f3f4d9d215f65a069df3d84e00365d0c6ae572
junki-hiroi <junki.hiroi@gmail.com>
2023-12-22 02:02:31 +0900

    Add カテゴリ説明ページ追加

---
+++ scripts/create_button_element/dataset_to_categories.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+require 'json'
+
+target_file_path = "#{__dir__}/../../src/dataset/categories.json"
+data = []
+
+Dir.glob("#{__dir__}/../../dataset/sounds/**.yml") do |filepath|
+  data << YAML.load_file(filepath)
+end
+
+data.flatten!
+
+categories = JSON.parse(File.read(target_file_path))
+data.each do |sound|
+  sound['category'].each do |category|
+    if category == ''
+      puts "category が空です: #{sound}"
+      next
+    end
+    init_data = {
+      description: "",
+    }
+    categories[category] = init_data unless categories.has_key?(category)
+  end
+end
+
+File.write(target_file_path, JSON.pretty_generate(categories.sort.to_h))
