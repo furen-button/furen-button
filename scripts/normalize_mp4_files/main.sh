35a1eec08ecafe9920a5c315eee951845cc06b08
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-09-29 20:29:43 +0900

    Add 動画のノーマライズスクリプト追加

---
+++ scripts/normalize_mp4_files/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+find ../../public/sounds | grep "\.mp4$" | grep -v "\-scaled\.mp4$" | sed s/\.mp4// > list.txt
+
+for file in `cat list.txt`
+do
+  ORIGINAL_FILE="${file}.mp4"
+  SCALED_FILE="${file}-scaled.mp4"
+  if [ -e "${SCALED_FILE}" ]; then
+    continue
+  fi
+  ffmpeg -i "${ORIGINAL_FILE}" -vf scale=320:240 -c:v libx264 -crf 23 "${SCALED_FILE}"
+done
