44d00eaacea4c0d2422d59bc933e7ac9386e9319
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-10-08 04:33:52 +0900

    Add getAnalytics

--- src/lib/FirebaseFunctions.ts
+++ src/lib/FirebaseFunctions.ts
@@ -1,10 +1,17 @@
 import {FirebaseApp, initializeApp} from 'firebase/app';
 import {Auth, getAuth, signInAnonymously} from 'firebase/auth';
 import {get, getDatabase, ref, set} from 'firebase/database';
+import {getAnalytics} from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA9Lsj7FTia1FLxlFxqx5h55dN293Kqick',
+  authDomain: 'furen-button.firebaseapp.com',
   databaseURL: 'https://furen-button-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'furen-button',
+  storageBucket: 'furen-button.appspot.com',
+  messagingSenderId: '58854415568',
+  appId: '1:58854415568:web:0b6925f559f4b9b59712d5',
+  measurementId: 'G-HBG03F74B0'
 };
 
 let app : FirebaseApp | null = null;
@@ -27,6 +34,7 @@ export const NullClapData : ClapData = {
 async function login() {
   app = initializeApp(firebaseConfig);
   auth = getAuth(app);
+  getAnalytics(app);
   await signInAnonymously(auth);
 }
 
