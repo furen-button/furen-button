e4b894dc7366233eca4329e6d83914cc09fdc4ca
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-10-07 23:54:53 +0900

    Add like button

--- src/components/SoundList.tsx
+++ src/components/SoundList.tsx
@@ -1,11 +1,12 @@
 import {SoundData} from './SoundData.tsx';
 import React, {lazy, Suspense} from 'react';
-import { FaPaperclip, FaVideo } from 'react-icons/fa6';
+import { FaVideo, FaPlay, FaThumbsUp } from 'react-icons/fa6';
+import {Button, ButtonGroup, IconButton, Popover} from '@mui/material';
 const VideoSourceLabel = lazy(() => import('./VideoSourceLabel.tsx'));
 
 export interface SoundListProps {
   filteredSoundDataList: SoundData[];
-  onClick: (event: React.MouseEvent<HTMLDivElement>, soundData: SoundData) => void;
+  onClick: (event: React.MouseEvent<HTMLElement>, soundData: SoundData) => void;
   selectedCategory: string[];
   playingSoundDataList: SoundData[];
   sectionPattern: 'ruby' | 'source';
@@ -89,7 +90,7 @@ function SoundList(props: SoundListProps) {
 
 function SoundButton(props: {
   soundData: SoundData;
-  onClick: (event: React.MouseEvent<HTMLDivElement>, soundData: SoundData) => void;
+  onClick: (event: React.MouseEvent<HTMLElement>, soundData: SoundData) => void;
   selectedCategory: string[];
   playingSoundDataList: SoundData[];
 }) {
@@ -100,74 +101,139 @@ function SoundButton(props: {
   const isPlaying = found !== undefined;
   const className = isPlaying ? 'sounds anime-button' : 'sounds';
 
-  const directory = 'sounds';
-
-  const details = <details>
-    <summary>詳細</summary>
-    <div style={style.detail}>
-      <ul className="context-list">
-        <li>
-          <div className="context-title">名前:</div>
-          <span>{soundData.name}</span>
-        </li>
-        <li>
-          <div className="context-title">ルビ:</div>
-          <span>{soundData.ruby}</span>
-        </li>
-        <li>
-          <div className="context-title">ファイル:</div>
-          <span><a href={`${directory}/${soundData.fileName}`}>{soundData.fileName}</a></span>
-        </li>
-        <li hidden={soundData.movieFileName === ''}>
-          <div className="context-title">動画ファイル:</div>
-          <span><a href={`${directory}/${soundData.movieFileName}`}>{soundData.movieFileName}</a></span>
-        </li>
-        <li>
-          <div className="context-title">カテゴリ:</div>
-          <span>{soundData.category}</span>
-        </li>
-        <li>
-          <div className="context-title">元動画:</div>
-          <a
-            target="_blank"
-            href={soundData.sourceUrl}>
-            {soundData.sourceName}
-          </a>
-        </li>
-        <li hidden={soundData.clipUrl === ''}>
-          <div className="context-title">クリップ:</div>
-          <a
-            target="_blank"
-            href={soundData.clipUrl}>
-            クリップリンク
-          </a>
-        </li>
-      </ul>
-    </div>
-  </details>;
-
   return (
-    <>
-      <div
-        className={className}
-        data-name={soundData.name}
-        data-ruby={soundData.ruby}
-        data-file={soundData.fileName}
-        data-movie-file={soundData.movieFileName}
-        data-source={soundData.sourceName}
-        data-source-url={soundData.sourceUrl}
-        data-category={soundData.category}
-        data-clip-url={soundData.clipUrl}
-        onClick={(event) => props.onClick(event, soundData)}
-      >
-        {soundData.movieFileName !== '' && <FaVideo style={{fontSize: '20px'}}/>}
-        {soundData.clipUrl !== '' && <FaPaperclip style={{fontSize: '20px'}}/>}
-        {soundData.name}
-      </div>
-      {details}
-    </>
+    <ButtonGroup className={className}>
+      <Button onClick={(event) => props.onClick(event, soundData)}>
+        <IconButton style={{color: '#1976d2'}}>
+          {soundData.movieFileName !== '' ? <FaVideo/> : <FaPlay/>}
+        </IconButton>
+        <span style={{
+          fontSize: '20px',
+          fontWeight: 'bold',
+        }}>
+          {soundData.name}
+        </span>
+      </Button>
+      <LikeButton targetId={soundData.fileName} likeCount={1000} localLikeCount={0} />
+      <DetailPopupButton soundData={soundData}/>
+    </ButtonGroup>
   );
 }
+
+function DetailPopupButton(props: { soundData: SoundData }) {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const soundData = props.soundData;
+  const directory = 'sounds';
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  return (
+    <>
+      <Button
+        onClick={handleClick}>詳細</Button>
+      <Popover
+        id={soundData.fileName}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+      >
+        <ul>
+          <li>
+            <div className="context-title">名前:</div>
+            <span>{soundData.name}</span>
+          </li>
+          <li>
+            <div className="context-title">ルビ:</div>
+            <span>{soundData.ruby}</span>
+          </li>
+          <li>
+            <div className="context-title">ファイル:</div>
+            <span><a href={`${directory}/${soundData.fileName}`}>{soundData.fileName}</a></span>
+          </li>
+          <li hidden={soundData.movieFileName === ''}>
+            <div className="context-title">動画ファイル:</div>
+            <span><a href={`${directory}/${soundData.movieFileName}`}>{soundData.movieFileName}</a></span>
+          </li>
+          <li>
+            <div className="context-title">カテゴリ:</div>
+            <span>{soundData.category}</span>
+          </li>
+          <li>
+            <div className="context-title">元動画:</div>
+            <a
+              target="_blank"
+              href={soundData.sourceUrl}>
+              {soundData.sourceName}
+            </a>
+          </li>
+          <li hidden={soundData.clipUrl === ''}>
+            <div className="context-title">クリップ:</div>
+            <a
+              target="_blank"
+              href={soundData.clipUrl}>
+                  クリップリンク
+            </a>
+          </li>
+        </ul>
+      </Popover>
+    </>);
+}
+
+/**
+ * いいねボタン
+ *
+ * @param props
+ * @param {string} props.targetId ターゲットのID
+ * @param {number} props.likeCount 累計いいねの数
+ * @param {number} props.localLikeCount 自分のいいねの数
+ * @constructor
+ */
+function LikeButton(props: { targetId: string, likeCount: number, localLikeCount: number }) {
+  const MAX_LIKE_COUNT = 3;
+  const likeCountReducer = (state: number, action: 'clap') => {
+    switch (action) {
+    case 'clap':
+      if (state >= MAX_LIKE_COUNT) {
+        return state;
+      }
+      return state + 1;
+    }
+  };
+  const [localLikeCount, updateLike] = React.useReducer(likeCountReducer, props.localLikeCount);
+  const color = localLikeCount === 0 ? '#1976d2' :
+    localLikeCount >= MAX_LIKE_COUNT ? 'red' : 'crimson';
+  return (
+    <Button
+      onClick={() => {
+        updateLike('clap');
+      }}
+    >
+      <IconButton
+        style={{color: color}}
+      >
+        <FaThumbsUp/>
+      </IconButton>
+      <div style={{
+        lineHeight: '1',
+        color: color,
+      }}>
+        <span>{localLikeCount}/{MAX_LIKE_COUNT}</span>
+        <br/>
+        <span>({props.likeCount + localLikeCount})</span>
+      </div>
+    </Button>
+  );
+}
+
 
 const style: { [key: string]: React.CSSProperties } = {
   container: {
@@ -189,10 +255,6 @@ const style: { [key: string]: React.CSSProperties } = {
     textAlign: 'center',
     fontSize: '30px',
     fontWeight: 'bold',
-  },
-  detail: {
-    border: 'solid 1px #aaa',
-    borderRadius: '4px',
   },
 };
 
