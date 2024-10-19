962dfb768a0e3d9b7f168d61737e46d0441b1aa9
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-10-20 04:20:02 +0900

    Update 総再生回数の表示追加

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx';
 import LatestFeeds from './components/LatestFeeds.tsx';
 import { install } from 'ga-gtag';
 import { Login } from './Login.tsx';
+import { AllPlayCount } from './components/AllPlayCount.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LatestFeeds />
     <Login>
+      <AllPlayCount />
       <App />
     </Login>
   </React.StrictMode>,
