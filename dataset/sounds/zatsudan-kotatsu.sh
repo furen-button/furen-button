0cc9675c18b02952812b83542f46cae6d1515673
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-11-02 14:54:10 +0900

    Add こたつ雑談

--- dataset/sounds/zatsudan-kotatsu.sh
+++ dataset/sounds/zatsudan-kotatsu.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
+rm -rf ./zatsudan-kotatsu.yml
+
 # ceylon-tea
 mkdir -p ../../public/sounds/zatsudan-kotatsu
 ffmpeg -y -ss 0:12:37.976 -i ../../scripts/video-clip-tool/public/movies/zatsudan-kotatsu.mp4 -t 0:00:00.632 ../../public/sounds/zatsudan-kotatsu/ceylon-tea.mp4
@@ -67,5 +69,71 @@ cat <<EOF >> ./zatsudan-kotatsu.yml
   fileName: "zatsudan-kotatsu/prince.mp3"
   category: ["emotions"]
   clipUrl: "https://www.youtube.com/watch?v=20m5OtnskxE&t=5098"
+EOF
+
+# baa
+mkdir -p ../../public/sounds/zatsudan-kotatsu
+ffmpeg -y -ss 0:01:08.025 -i ../../scripts/video-clip-tool/public/movies/zatsudan-kotatsu.mp4 -t 0:00:01.888 ../../public/sounds/zatsudan-kotatsu/baa.mp4
+ffmpeg -y -i ../../public/sounds/zatsudan-kotatsu/baa.mp4 -vn ../../public/sounds/zatsudan-kotatsu/baa.wav
+ffmpeg -y -f lavfi -i 'color=c=black:s=1080x1920:r=30000/1001:d=1.8884160000000065' -f lavfi -i 'aevalsrc=0|0:c=stereo:s=44100:d=1.8884160000000065' ../../public/sounds/zatsudan-kotatsu/base.mp4
+
+
+# name: ばぁ
+# ruby: ばぁ
+# category: se
+# clipUrl: https://www.youtube.com/watch?v=20m5OtnskxE&t=68
+#
+touch ./zatsudan-kotatsu.yml
+cat <<EOF >> ./zatsudan-kotatsu.yml
+- name: "ばぁ"
+  ruby: "ばぁ"
+  source: "zatsudan-kotatsu"
+  fileName: "zatsudan-kotatsu/baa.mp3"
+  category: ["se"]
+  clipUrl: "https://www.youtube.com/watch?v=20m5OtnskxE&t=68"
+EOF
+
+# miteruyo
+mkdir -p ../../public/sounds/zatsudan-kotatsu
+ffmpeg -y -ss 0:01:04.552 -i ../../scripts/video-clip-tool/public/movies/zatsudan-kotatsu.mp4 -t 0:00:00.777 ../../public/sounds/zatsudan-kotatsu/miteruyo.mp4
+ffmpeg -y -i ../../public/sounds/zatsudan-kotatsu/miteruyo.mp4 -vn ../../public/sounds/zatsudan-kotatsu/miteruyo.wav
+ffmpeg -y -f lavfi -i 'color=c=black:s=1080x1920:r=30000/1001:d=0.7776860000000028' -f lavfi -i 'aevalsrc=0|0:c=stereo:s=44100:d=0.7776860000000028' ../../public/sounds/zatsudan-kotatsu/base.mp4
+
+
+# name: 見てるよ
+# ruby: みてるよ
+# category: miteruyo
+# clipUrl: https://www.youtube.com/watch?v=20m5OtnskxE&t=64
+#
+touch ./zatsudan-kotatsu.yml
+cat <<EOF >> ./zatsudan-kotatsu.yml
+- name: "見てるよ"
+  ruby: "みてるよ"
+  source: "zatsudan-kotatsu"
+  fileName: "zatsudan-kotatsu/miteruyo.mp3"
+  category: ["miteruyo"]
+  clipUrl: "https://www.youtube.com/watch?v=20m5OtnskxE&t=64"
+EOF
+
+# yahho
+mkdir -p ../../public/sounds/zatsudan-kotatsu
+ffmpeg -y -ss 0:01:19.250 -i ../../scripts/video-clip-tool/public/movies/zatsudan-kotatsu.mp4 -t 0:00:01.121 ../../public/sounds/zatsudan-kotatsu/yahho.mp4
+ffmpeg -y -i ../../public/sounds/zatsudan-kotatsu/yahho.mp4 -vn ../../public/sounds/zatsudan-kotatsu/yahho.wav
+ffmpeg -y -f lavfi -i 'color=c=black:s=1080x1920:r=30000/1001:d=1.121020999999999' -f lavfi -i 'aevalsrc=0|0:c=stereo:s=44100:d=1.121020999999999' ../../public/sounds/zatsudan-kotatsu/base.mp4
+
+
+# name: やっほー
+# ruby: やっほー
+# category: emotions
+# clipUrl: https://www.youtube.com/watch?v=20m5OtnskxE&t=79
+#
+touch ./zatsudan-kotatsu.yml
+cat <<EOF >> ./zatsudan-kotatsu.yml
+- name: "やっほー"
+  ruby: "やっほー"
+  source: "zatsudan-kotatsu"
+  fileName: "zatsudan-kotatsu/yahho.mp3"
+  category: ["emotions"]
+  clipUrl: "https://www.youtube.com/watch?v=20m5OtnskxE&t=79"
 EOF
 
