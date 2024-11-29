85e65d710eeff05f46d40a99e6b0094b45f8f044
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-11-30 00:18:09 +0900

    Revert "Update 待機秒数を伸ばす"
    This reverts commit d766544ef346f84b7d226700373802ff5b818850.

--- scripts/twitter_post_bot/app.ts
+++ scripts/twitter_post_bot/app.ts
@@ -33,7 +33,6 @@ interface SoundData {
 }
 
 const soundDataList : SoundData[] = JSON.parse(fs.readFileSync('../../public/dataset/sounds.json', 'utf-8'));
-const WAIT_TIME = 20000;
 
 /**
  * ランダムでサウンドデータを取得する
@@ -118,7 +117,7 @@ async function postTweetThread(soundDataList: SoundData[]) {
                 in_reply_to_tweet_id: lastTweet.data?.id
             };
         }
-        await new Promise((resolve) => setTimeout(resolve, WAIT_TIME));
+        await new Promise((resolve) => setTimeout(resolve, 5000));
         const tweet = await client.v2.tweet(param);
         postedTweets.push(tweet);
     }
