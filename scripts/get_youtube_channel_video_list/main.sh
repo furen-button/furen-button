ba651997f7b00c16e1e2ab7d97529041cf41e3b6
hiroi_junki <junki.hiroi@drecom.co.jp>
2025-01-06 18:31:09 +0900

    Add 2025 年の収集追加

--- scripts/get_youtube_channel_video_list/main.sh
+++ scripts/get_youtube_channel_video_list/main.sh
@@ -20,3 +20,6 @@ cp result.csv ../../dataset/videolists/furen_2023.json
 
 npm start 'part=snippet&channelId=UCuep1JCrMvSxOGgGhBfJuYw&order=date&type=video&maxResults=50&safeSearch=none&publishedBefore=2024-12-31T00:00:00Z&publishedAfter=2023-12-28T00:00:00Z'
 cp result.csv ../../dataset/videolists/furen_2024.json
+
+npm start 'part=snippet&channelId=UCuep1JCrMvSxOGgGhBfJuYw&order=date&type=video&maxResults=50&safeSearch=none&publishedBefore=2025-12-31T00:00:00Z&publishedAfter=2024-12-28T00:00:00Z'
+cp result.csv ../../dataset/videolists/furen_2025.json
